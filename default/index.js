8df3b113d0adda0b15e570bf5f65f30534833872
Nicolas Hery <hery.nicolas@gmail.com>
2014-08-07 13:33:53 -0400

    Add webpack compatibility

---
+++ default/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  user: require('./user.json'),
+  patients: {
+    '11': require('./patients/11.json'),
+    '21': require('./patients/21.json'),
+    '31': require('./patients/31.json'),
+    '41': require('./patients/41.json')
+  },
+  patientdata: {
+    '11': require('./patientdata/11.json')
+  },
+  messagethread: {
+    tbd: require('./messagethread/tbd.json')
+  },
+  messagenotes: {
+    '11': require('./messagenotes/11.json')
+  }
+};
